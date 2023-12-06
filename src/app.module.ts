eb29fa4b7db72a142c50a84693519224f1fbe990
murilogoedert <murilo.goedert@omnismart.com.br>
2023-12-06 02:01:34 -0300

    enable prometheus

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MainController } from './presentation/controllers/main.controller';
 import { MainModule } from './modules/main.module';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [MainModule],
+  imports: [MainModule, PrometheusModule.register()],
   controllers: [MainController],
   providers: [],
 })
